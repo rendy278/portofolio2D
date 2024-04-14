3b24bf87513c521d862a8ea2bd1e4286b2a64a9a
rendy278 <kepolaumautauaja@gmail.com>
2024-04-15 04:01:59 +0700

    fix target url tv

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const dialogueData = {
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
 
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube Tutorial Web Programming Channel.`,
-  tv: `That's my TV. I like watching web development technology on YouTube such as <a href="www.youtube.com/@sandhikagalihWPU" target="_blank">Web Programming Unpas</a>, <a href="www.youtube.com/@deaafrizal" target="_blank">Dea Afrizal</a> and <a href="www.youtube.com/@ProgrammerZamanNow" target="_blank">Programmer Zaman Now</a> to increase my knowledge about web development technology.`,
+  tv: `That's my TV. I like watching web development technology on YouTube such as <a href="https://www.youtube.com/@sandhikagalihWPU" target="_blank">Web Programming Unpas</a>, <a href="https://www.youtube.com/@deaafrizal" target="_blank">Dea Afrizal</a>, and <a href="https://www.youtube.com/@ProgrammerZamanNow" target="_blank">Programmer Zaman Now</a> to increase my knowledge about web development technology.`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/rendy278" target="_blank">Check it out?</a>
   Contact me at rendyyoshizawa@gmail.com if you have any interesting job opportunities!`,
