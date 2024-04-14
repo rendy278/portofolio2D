8d0a1ecd3e19028725169e88a7c878d71644cd44
rendy278 <kepolaumautauaja@gmail.com>
2024-04-15 03:37:43 +0700

    first commit

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/rendy278" target="_blank">Github</a>!`,
+
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube Tutorial Web Programming Channel.`,
+  tv: `That's my TV. I like watching web development technology on YouTube such as <a href="www.youtube.com/@sandhikagalihWPU" target="_blank">Web Programming Unpas</a>, <a href="www.youtube.com/@deaafrizal" target="_blank">Dea Afrizal</a> and <a href="www.youtube.com/@ProgrammerZamanNow" target="_blank">Programmer Zaman Now</a> to increase my knowledge about web development technology.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/rendy278" target="_blank">Check it out?</a>
+  Contact me at rendyyoshizawa@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are many programming books on my shelf. There's even one in Indonesian (I also speak Indonesian btw).
+  I may only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to leave, would you like to visit my main <a href="https://rendev-ten.vercel.app/" target="_blank">Portofolio Website</a>?`,
+};
